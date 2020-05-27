0ae268a8208950fd2cf8f53a3392c134821e5f83
TWenzel1 <48931883+TWenzel1@users.noreply.github.com>
2020-05-27 18:17:21 -0500

    update

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -44,7 +44,7 @@ namespace WebApplication1
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
